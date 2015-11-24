d911e1e948cfc78f00a6c072374642a4ffc66581
Sudheer Koganti <skoganti@codeaurora.org>
2015-11-24 09:24:23 -0800

    Fix issue with webview not ready
    Some times, webview can return 0 for getWidth().  Guard against this
    when taking a webview snapshot.
    
    Change-Id: Id4d40aa90afcb86860237eac084c082b2bc012aa

--- src/com/android/browser/PhoneUi.java
+++ src/com/android/browser/PhoneUi.java
@@ -291,7 +291,7 @@ public class PhoneUi extends BaseUi {
 
         Bitmap sbm;
         WebView webView = getWebView();
-        if (webView != null) {
+        if (webView != null && webView.getWidth() != 0) {
             int view_width = webView.getWidth();
             int capture_width = mActivity.getResources().getDimensionPixelSize(
                     R.dimen.tab_thumbnail_width);
